a3c4547a259956cd24288e2e2262190d90e7a1fc
Urielminpro705 <urielmata37@hotmail.com>
2024-10-05 23:00:21 -0600

    Mejoras en la barra de busqueda

--- JS/busqueda.js
+++ JS/busqueda.js
@@ -5,15 +5,21 @@ $(document).ready(function() {
     function openAndClose() {
         if (isOpenPanelFilter) {
             filter_btn.removeClass("filter-btn-activated")
-            $(".cont-search-input").removeClass("cont-search-input-activated")
-            $("#filter-btn-x-icon").css("display","none")
-            $("#filter-btn-filter-icon").css("display","block")
+            setTimeout(function (){
+                $(".cont-search-input").removeClass("cont-search-input-activated")
+                $("#filter-btn-x-icon").css("display","none")
+                $("#filter-btn-filter-icon").css("display","block")
+            },100)
+            $(".cont-filters").removeClass("cont-filters-activated")
             isOpenPanelFilter = false
         } else {
             filter_btn.addClass("filter-btn-activated")
             $(".cont-search-input").addClass("cont-search-input-activated")
             $("#filter-btn-x-icon").css("display","block")
             $("#filter-btn-filter-icon").css("display","none")
+            setTimeout(function (){
+                $(".cont-filters").addClass("cont-filters-activated")
+            },200)
             isOpenPanelFilter = true
         }
     }
